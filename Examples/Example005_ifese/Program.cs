81956b732ba516fd6ef470117a87d840105cd84b
Mihail <tamsjam@mail.ru>
2023-01-18 18:58:06 +0300

    Example005 ifese csproj

---
+++ Examples/Example005_ifese/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя ползователя ");
+string usernam = Console.ReadLine();
+
+if(usernam.ToLower() == "маша")
+{
+    Console.WriteLine("Ура это же МАША");
+}
+    else
+    {
+        Console.Write("Привет, ");
+        Console.WriteLine(usernam);
+    }
